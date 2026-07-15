58c4b4bafa2fba4c057701858de4f5d3a83304d0
S. Mondal <soura.html@gmail.com>
2026-07-15 11:56:44 +0530

    Add files via upload

---
+++ notes/python/python002.py
@@ -0,0 +1,23 @@
+"""
+name = input("Enter your name:")
+print("Welcome", name)
+age = input("Enter your age:")
+print("okay! you are", age)
+age1 = float(input("Enter your age:"))
+print(type(age1))
+"""
+
+# a = float(input("number 1:"))
+# b = float(input("numper 2:"))
+# sum = ( a+b )
+# print ( sum )
+# print ("here is your avarage:", sum/2 ) 
+# if a >= b :
+#     print (True)
+# else:
+#     print (False)
+
+
+# x = float(input(" input the size of a side of a square:"))
+# print("cool!", "here is the area:",x**2 )
+
